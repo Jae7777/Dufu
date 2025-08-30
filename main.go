f627e4dbc900081c08f14966d344feb4dd56ba94
Justin Chen <jchen4086@gmail.com>
2025-08-30 14:10:45 -0400

    bot joins voice channel

--- main.go
+++ main.go
@@ -34,8 +34,8 @@ func main() {
 	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
 
-	// In this example, we only care about receiving message events.
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	// In this example, we only care about receiving message events and voice state changes.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuildVoiceStates
 
 	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
@@ -73,4 +73,57 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Content == "pong" {
 		s.ChannelMessageSend(m.ChannelID, "Ping!")
 	}
+
+	// If the message is "!join", join the voice channel of the user who sent the message.
+	if m.Content == "!join" {
+		// Find the guild for that channel.
+		g, err := s.State.Guild(m.GuildID)
+		if err != nil {
+			fmt.Println("Error finding guild:", err)
+			return
+		}
+
+		// Find the voice state for the user who sent the message.
+		vs := findUserVoiceState(g, m.Author.ID)
+		if vs == nil {
+			s.ChannelMessageSend(m.ChannelID, "You are not in a voice channel.")
+			return
+		}
+
+		// Join the voice channel.
+		_, err = s.ChannelVoiceJoin(g.ID, vs.ChannelID, false, true)
+		if err != nil {
+			fmt.Println("Error joining voice channel:", err)
+			return
+		}
+
+		s.ChannelMessageSend(m.ChannelID, "Joined your voice channel!")
+	}
+
+	// If the message is "!leave", leave the current voice channel.
+	if m.Content == "!leave" {
+		vc, ok := s.VoiceConnections[m.GuildID]
+		if !ok {
+			s.ChannelMessageSend(m.ChannelID, "I'm not in a voice channel.")
+			return
+		}
+
+		err := vc.Disconnect()
+		if err != nil {
+			fmt.Println("Error disconnecting from voice channel:", err)
+			return
+		}
+
+		s.ChannelMessageSend(m.ChannelID, "Left the voice channel.")
+	}
+}
+
+// Helper function to find a user's voice state in a guild.
+func findUserVoiceState(g *discordgo.Guild, userID string) *discordgo.VoiceState {
+	for _, vs := range g.VoiceStates {
+		if vs.UserID == userID {
+			return vs
+		}
+	}
+	return nil
 }
